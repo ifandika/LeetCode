0f8814a2ea08d09c1c5fb25d0693a281a8da5fe7
ifandika <ifandika014@gmail.com>
2024-11-25 13:31:19 +0700

    update problemset

--- Problemset/Easy-Level/E70_ClimbingStairs.java
+++ Problemset/Easy-Level/E70_ClimbingStairs.java
@@ -1,35 +1,58 @@
-// - Dibutuhkan langkah 'n' untuk menaiki tangga.
-// - Setiap kali menaiki 1 atau 2 anak tangga.
-// - Banyak cara naik untuk sampai ke puncak.
-// Contoh 1:
-// Input: 5
-// Output:
-// 1. 1 + 1 + 1 + 1 + 1
-// 2. 1 + 1 + 1 + 2
-// 3. 1 + 2 + 2
-// 4. 2 + 2 + 1
-// 5. 2 + 1 + 1 + 1
-// 6. 1 + 2 + 1 + 1
-// 7. 2 + 1 + 2
-// 8. 1 + 1 + 2 + 1
+/**
+ * https://leetcode.com/problems/climbing-stairs/
+ * 
+ * - Dibutuhkan langkah 'n' untuk menaiki tangga.
+ * - Setiap kali menaiki 1 atau 2 anak tangga.
+ * - Banyak cara naik untuk sampai ke puncak.
+ * 
+ * Contoh 1:
+ * Input: 5
+ * Output:
+ * 1. 1 + 1 + 1 + 1 + 1
+ * 2. 1 + 1 + 1 + 2
+ * 3. 1 + 2 + 2
+ * 4. 2 + 2 + 1
+ * 5. 2 + 1 + 1 + 1
+ * 6. 1 + 2 + 1 + 1
+ * 7. 2 + 1 + 2
+ * 8. 1 + 1 + 2 + 1
+ * 
+ * Solusi 1: Brute Force
+ * Bisa menggunakan fibonaci.
+ * Solusi 2: Dynamic Programming
+ * Menggunakan 
+ * Solusi 3:
+ * Menggunakan matriks eksponen
+ */
 
-// Solusi 1: Brute Force
-// Bisa menggunakan fibonaci.
-
-// Solusi 2: Dynamic Programming
-// Menggunakan 
-
-// Solusi 3:
-// Menggunakan matriks eksponen
-
+import java.util.*;
 
 public class E70_ClimbingStairs {
-	public int climbStair(int n) {
-		if(n < 2) {
-			return 1;
-		}
-		else {
-			return climbStair(n-1) + climbStair(n-2);
-		}
+    public static void main(String[] args) {
+        // Test-1
+
+        int n = 7;
+        System.out.println(solution(n));
+    }
+
+	/**
+     * 
+     * 
+     */
+    public static int solution(int n) {
+		if(n <= 3) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+            System.out.println(Arrays.toString(dp));
+        }
+
+        return dp[n];  
 	}
 }
