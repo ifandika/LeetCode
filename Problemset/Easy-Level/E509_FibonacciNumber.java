03e7896783e4daf9eeea0c6286868e4ed182670f
ifandika <ifandika014@gmail.com>
2024-11-30 14:02:56 +0700

    update problemset

--- Problemset/Easy-Level/E509_FibonacciNumber.java
+++ Problemset/Easy-Level/E509_FibonacciNumber.java
@@ -9,7 +9,13 @@ import java.util.*;
 
 public class E509_FibonacciNumber {
     public static void main(String[] args) {
-        
+        // Test-1
+        int n = 2;
+        System.out.println("Test-1: "+solution(n));
+
+        // Test-2
+        n = 20;
+        System.out.println("Test-2: "+solution(n));
     }
 
     /**
@@ -17,7 +23,7 @@ public class E509_FibonacciNumber {
      * 
      */
     public static int solution(int n) {
-        if(n = 1) return 1;
+        if(n == 1) return 1;
         if(n <= 0) return 0;
         return solution(n-1) + solution(n-2);
     }
@@ -25,7 +31,7 @@ public class E509_FibonacciNumber {
     /**
      * Ultimate Solution
      */
-    public static int solution(int n) {
+    public static int solution2(int n) {
         if (n < 2) return n;
 
         double sqrt5 = Math.sqrt(5);
