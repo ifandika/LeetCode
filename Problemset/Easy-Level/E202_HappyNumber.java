201084de715f74d4a6b29a1f90163a65d81efd3b
ifandika <ifandika014@gmail.com>
2024-12-02 17:29:12 +0700

    update problemset

--- Problemset/Easy-Level/E202_HappyNumber.java
+++ Problemset/Easy-Level/E202_HappyNumber.java
@@ -21,6 +21,18 @@ public class E202_HappyNumber {
         System.out.println(solution(n));
     }
 
+    /**
+     * - Menggunakan HasSet untuk menyimpan nilai, dengan hasset maka nilai
+     *   tidak boleh sama
+     * - perulangan while dengan kondisi true
+     * - buat fungsi squareSum yaitu memcah nilai menjadi satuan lalu menguadratkan
+     *   lalu dioperasikan, kembalikan hasil sum
+     * - n kita tetapkan nilai dengan fungsi tersebut
+     * - cek jika nilai n = 1 maka selesai/true/happy number
+     * - jika tidak maka kita cek pada HashSet apakah ada nilai tersebut, jika benar
+     *   maka perulangan tanpa henti, maka kita stop return false
+     * - untuk setiap perulangan kita masukan nilai n ke HashSet
+     */
 	public static boolean solution(int n) {
         HashSet<Integer> hashSet = new HashSet<Integer>();
         while(true) {
