9f520f4cdfa09d43de75f22dadc63efe9d75aae5
ifandika <ifandika014@gmail.com>
2025-02-24 10:27:12 +0700

    update problemset

--- Problemset/Easy-Level/E70_ClimbingStairs.java
+++ Problemset/Easy-Level/E70_ClimbingStairs.java
@@ -1,10 +1,10 @@
 /**
  * https://leetcode.com/problems/climbing-stairs/
- * 
+ *
  * - Dibutuhkan langkah 'n' untuk menaiki tangga.
  * - Setiap kali menaiki 1 atau 2 anak tangga.
  * - Banyak cara naik untuk sampai ke puncak.
- * 
+ *
  * Contoh 1:
  * Input: 5
  * Output:
@@ -16,78 +16,80 @@
  * 6. 1 + 2 + 1 + 1
  * 7. 2 + 1 + 2
  * 8. 1 + 1 + 2 + 1
- * 
+ *
  * Solusi 1: Brute Force
  * Bisa menggunakan fibonaci.
  * Solusi 2: Dynamic Programming
- * Menggunakan 
+ * Menggunakan
  * Solusi 3:
  * Menggunakan matriks eksponen
  */
-
 import java.util.*;
 
 public class E70_ClimbingStairs {
-    public static void main(String[] args) {
-        // Test-1
+  public static void main(String[] args) {
+    // Test-1
+    int n = 7;
+    System.out.println(solution(n));
+  }
 
-        int n = 7;
-        System.out.println(solution(n));
+  /**
+   * Hint - Fibonacci
+   *
+   * - Kita perlu tahu untuk setiap n punya berapa langkah
+   * - Contoh kemungkinan langkah n = 3
+   *      n = 3
+   *      - 1 + 1 + 1
+   *      - 1 + 2
+   *      - 2 + 1
+   *      (3 kemungkinan menaiki tangga 3)
+   * - Lalau untuk mencari tahu n = n-1 + n-2
+   * - Dimana yang kita jumlahkan adalah total langkah
+   * - Contoh:
+   *      n = 4 ?
+   *      n4 = n3 + n2
+   *      dimana langkah n = 3 adalah 3 kemungkinan
+   *      dimana langkah n = 2 adalah 2 kemungkinan
+   *      maka n4 = 3 + 2
+   *              = 5 kemungkinan
+   * - Untuk kemungkinan langkah
+   * n    = 1  2  3  4  5  6  7
+   * step = 1  2  3  5  8  13 21
+   */
+  public static int solution(int n) {
+    if (n <= 3) {
+      return n;
     }
+    int[] everyStepN = new int[n + 1];
 
-	/**
-     * Hint - Fibonacci
-     * 
-     * - Kita perlu tahu untuk setiap n punya berapa langkah
-     * - Contoh kemungkinan langkah n = 3
-     *      n = 3
-     *      - 1 + 1 + 1
-     *      - 1 + 2
-     *      - 2 + 1
-     *      (3 kemungkinan menaiki tangga 3)
-     * - Lalau untuk mencari tahu n = n-1 + n-2
-     * - Dimana yang kita jumlahkan adalah total langkah
-     * - Contoh: 
-     *      n = 4 ?
-     *      n4 = n3 + n2
-     *      dimana langkah n = 3 adalah 3 kemungkinan
-     *      dimana langkah n = 2 adalah 2 kemungkinan
-     *      maka n4 = 3 + 2
-     *              = 5 kemungkinan
-     * - Untuk kemungkinan langkah
-     * n    = 1  2  3  4  5  6  7
-     * step = 1  2  3  5  8  13 21
-     */
-    public static int solution(int n) {
-		if(n <= 3) { return n; }
-        int[] everyStepN = new int[n + 1];
+    // Langkah dasar n sampai 3
+    everyStepN[0] = 1;
+    everyStepN[1] = 2;
+    everyStepN[2] = 3;
 
-        // Langkah dasar n sampai 3
-        everyStepN[0] = 1;
-        everyStepN[1] = 2;
-        everyStepN[2] = 3;
-
-        // Kita mulai dari 4
-        for (int i = 3; i <= n; i++) {
-            everyStepN[i] = everyStepN[i - 1] + everyStepN[i - 2];
-        }
-        return everyStepN[n-1];
-	}
-
-    // Lebih mudah karena kita tidak pakai array index 0
-    public static int solution2(int n) {
-        if(n <= 3) { return n; }
-        int[] everyStepN = new int[n + 1];
-
-        // Langkah dasar n sampai 3
-        everyStepN[1] = 1;
-        everyStepN[2] = 2;
-        everyStepN[3] = 3;
-
-        // Kita mulai dari 4
-        for (int i = 4; i <= n; i++) {
-            everyStepN[i] = everyStepN[i - 1] + everyStepN[i - 2];
-        }
-        return everyStepN[n];
+    // Kita mulai dari 4
+    for (int i = 3; i <= n; i++) {
+      everyStepN[i] = everyStepN[i - 1] + everyStepN[i - 2];
     }
+    return everyStepN[n - 1];
+  }
+
+  // Lebih mudah karena kita tidak pakai array index 0
+  public static int solution2(int n) {
+    if (n <= 3) {
+      return n;
+    }
+    int[] everyStepN = new int[n + 1];
+
+    // Langkah dasar n sampai 3
+    everyStepN[1] = 1;
+    everyStepN[2] = 2;
+    everyStepN[3] = 3;
+
+    // Kita mulai dari 4
+    for (int i = 4; i <= n; i++) {
+      everyStepN[i] = everyStepN[i - 1] + everyStepN[i - 2];
+    }
+    return everyStepN[n];
+  }
 }
--- Problemset/Medium-Level/M46_Permutations.java
+++ Problemset/Medium-Level/M46_Permutations.java
@@ -1,58 +1,87 @@
 /**
  * https://leetcode.com/problems/permutations
- * 
+ *
  * - Diberikan nilai [1,2,3]
  * - Buat semua kemungkinan dari nilai
  * - Output = [[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
- * 
+ *
  * Klu
  * - Permutasi & Kombinasi dalam matematika
  * - Nilai awal diturak dengan semua jenis nilai selain nilai awal
  */
 
 import java.util.*;
+import static java.lang.System.*;
 
 public class M46_Permutations {
-    public static void main(String[] args) {
-        // Test-1
-        int[] nums = {1, 2, 3};
-        List<List<Integer>> result = solution(nums);
-        System.out.println(result);
-    }
+  public static void main(String[] args) {
+    // Test-1
+    int[] nums = { 1, 2, 3, 4 };
+    List<List<Integer>> result = solution2(nums);
+    System.out.println(result);
+  }
 
-    /**
-     * 
-     */
-    public static List<List<Integer>> solution(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        permutation(result, nums, new ArrayList<>());
-        return result;
-    }
+  // Solution 1
+  /**
+   *
+   */
+  public static List<List<Integer>> solution(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+    permutation(result, nums, new ArrayList<>());
+    return result;
+  }
 
-    /**
-     * - ambil panjang array untuk batas jelajah
-     * - jika ukuran {@code tempList} = panjang array maka kita masukan hasil {@code tempList} ke {@code list}
-     * - jika tidak kita for dari 0 - panjang array
-     * - lalu kita cek jika {@code tempList} berisi nilai array {@code nums} maka kita lompati karena kita perlu kombinasi
-     * - lalu masukan {@code nums} ke {@code tempList}
-     * - lalu rekursif kita panggil fungsi lagi, karena kombinasi jadi kita mulai dari awal, dan jika nilai sudah dimasukan kita lewati
-     *   untuk kita ambil nilai selanjutnya
-     * - 
-     */
-    public static void permutation(List<List<Integer>> list, int[] nums, ArrayList<Integer> tempList) {
-        int length = nums.length;
-        if(tempList.size() == length) {
-            System.out.println(" = "+tempList);
-            list.add(new ArrayList<Integer>(tempList));
-        }
-        else {
-            for(int i = 0; i < length; i++) {
-                if(tempList.contains(nums[i])) continue;
-                tempList.add(nums[i]);
-                permutation(list, nums, tempList);
-                System.out.println("=> "+tempList.get(tempList.size() - 1));
-                tempList.remove(tempList.size() - 1); // Remove same/duplicate number
-            }
-        }
+  /**
+   * - ambil panjang array untuk batas jelajah
+   * - jika ukuran {@code tempList} = panjang array maka kita masukan hasil {@code tempList} ke {@code list}
+   * - jika tidak kita for dari 0 - panjang array
+   * - lalu kita cek jika {@code tempList} berisi nilai array {@code nums} maka kita lompati karena kita perlu kombinasi
+   * - lalu masukan {@code nums} ke {@code tempList}
+   * - lalu rekursif kita panggil fungsi lagi, karena kombinasi jadi kita mulai dari awal, dan jika nilai sudah dimasukan kita lewati
+   *   untuk kita ambil nilai selanjutnya
+   * -
+   */
+  public static void permutation(List<List<Integer>> list, int[] nums, ArrayList<Integer> tempList) {
+    int length = nums.length;
+    if(tempList.size() == length) {
+      System.out.println(" = " + tempList);
+      list.add(new ArrayList<Integer>(tempList));
     }
+    else {
+      for (int i = 0; i < length; i++) {
+        if (tempList.contains(nums[i])) continue;
+        tempList.add(nums[i]);
+        permutation(list, nums, tempList);
+        System.out.println("=> " + tempList.get(tempList.size() - 1));
+        tempList.remove(tempList.size() - 1); // Remove same/duplicate number
+      }
+    }
+  }
+
+  // Solution 2
+  public static List<List<Integer>> solution2(int[] nums) {
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> temp = new ArrayList<>();
+    boolean[] freq = new boolean[nums.length];
+    oprtSolution2(nums, ans, temp, freq);
+    return ans;
+  }
+
+  public static void oprtSolution2(int[] nums, List<List<Integer>> ans, List<Integer> temp, boolean[] freq) {
+    if (temp.size() == nums.length) {
+      ans.add(new ArrayList<>(temp));
+      return;
+    }
+    for (int i = 0; i < nums.length; i++) {
+      if (!freq[i]) {
+        freq[i] = true;
+        temp.add(nums[i]);
+        out.println(Arrays.toString(temp.toArray()));
+        oprtSolution2(nums, ans, temp, freq);
+        System.out.println("=> " + temp.get(temp.size() - 1));
+        temp.remove(temp.size() - 1);
+        freq[i] = false;
+      }
+    }
+  }
 }
